943a77c14d2efa06365562c3bfdfcc1f14195d70
Dinesh <apiit.dinesh@gmail.com>
2021-07-31 12:49:46 +0800

    add: musician & admin pages and controller redirect to the pages

---
+++ CMM/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMM.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult CreateVirtualEvent()
+        {
+            return View();
+        }
+
+        public IActionResult EditVirtualEvent()
+        {
+            return View();
+        }
+
+        public IActionResult ListVirtualEvent()
+        {
+            return View();
+        }
+        public IActionResult CreateMusicianAccount()
+        {
+            return View();
+        }
+        public IActionResult ListMusician()
+        {
+            return View();
+        }
+
+        public IActionResult AdminEditMusicianAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMM/Controllers/MusicianController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMM.Controllers
+{
+    public class MusicianController : Controller
+    {
+        public IActionResult MusicianViewUpcomingEvents()
+        {
+            return View();
+        }
+
+        public IActionResult MusicianEditAccount()
+        {
+            return View();
+        }
+    }
+}
