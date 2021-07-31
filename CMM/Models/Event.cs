8bb39850cabd105373c593e65bff924cc25dc32b
Dinesh <apiit.dinesh@gmail.com>
2021-07-31 15:09:24 +0800

    add: event model

---
+++ CMM/Models/Event.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMM.Models
+{
+    public class Event
+    {
+        [Key]
+        public int ConcertID { get; set; }
+
+        [Key]
+        public string ConcertPoster { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string ConcertMusician { get; set; }
+
+        [Required]
+        [Url]
+        public string ConcertLink { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string ConcertName { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string ConcertDescription { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ConcertDateTime { get; set; }
+
+        [Required]
+        [Range(0, 999.99)]
+        public decimal ConcertPrice { get; set; }
+
+        [Required]
+        public int TicketLimit { get; set; }
+
+        [Required]
+        public string ConcertStatus {get; set;}
+
+        [Required]
+        public Boolean ConcertVisibility { get; set; }
+    }
+}
