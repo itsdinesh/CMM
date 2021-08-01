d8059e42c5fb9919a626f1606d845818263583d1
Dinesh <37468808+itsdinesh@users.noreply.github.com>
2021-08-01 18:45:52 +0800

    add: user functions for view event & checkout (frontend)

--- CMM/Controllers/UserController.cs
+++ CMM/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CMM.Data;
+using CMM.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +13,72 @@ namespace CMM.Controllers
     [Authorize(Roles = "Patron")]
     public class UserController : Controller
     {
-        public IActionResult UserViewUpcomingEvents()
+        private readonly CMMEventContext _context;
+
+        public UserController(CMMEventContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> UserViewUpcomingEvents()
+        {
+            return View(await _context.Event.ToListAsync());
         }
 
-        public IActionResult PurchaseTicket()
+        public async Task<IActionResult> PurchaseTicket(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.ConcertID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        public async Task<IActionResult> Checkout(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Checkout(int id, [Bind("ConcertID,ConcertPoster,ConcertMusician,ConcertLink,ConcertName,ConcertDescription,ConcertDateTime,ConcertPrice,TicketLimit,ConcertStatus,ConcertVisibility")] Event @event)
+        {
+            if (id != @event.ConcertID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
         }
 
         public IActionResult TicketHistory()
