a63fd2acba72e980a86c1f4505f843a5c952b4e4
Dinesh <37468808+itsdinesh@users.noreply.github.com>
2021-08-03 19:59:20 +0800

    add: event page for musician & fix event page for guest

--- CMM/Controllers/MusicianController.cs
+++ CMM/Controllers/MusicianController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CMM.Areas.Identity.Data;
+using CMM.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +14,43 @@ namespace CMM.Controllers
     [Authorize(Roles = "Musician")]
     public class MusicianController : Controller
     {
-        public IActionResult MusicianViewUpcomingEvents()
+        private readonly CMMEventContext _context;
+        private readonly UserManager<CMMUser> _userManager;
+
+        public MusicianController(CMMEventContext contextEvent, UserManager<CMMUser> userManager)
         {
-            return View();
+            _context = contextEvent;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> MusicianViewUpcomingEvents()
+        {
+            string concertMusician = (await _userManager.GetUserAsync(User))?.Name;
+            ViewBag.concertMusician = concertMusician;
+
+            return View(await _context.Event.ToListAsync());
         }
 
         public IActionResult MusicianEditAccount()
         {
             return View();
+        }
+
+        public async Task<IActionResult> MusicianEventDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.ConcertID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
         }
     }
 }
